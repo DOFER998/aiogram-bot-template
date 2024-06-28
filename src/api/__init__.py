bc9f84d6a12a73a58949999f53a2be877c5dc546
c0mradΞ.eth <67563470+DOFER998@users.noreply.github.com>
2024-06-28 22:24:03 +0600

    feat: add error handler

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from aiogram.exceptions import TelegramBadRequest
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 from .routers import root_router, webhook_router
 
@@ -6,6 +9,15 @@ app = FastAPI()
 
 app.include_router(root_router)
 app.include_router(webhook_router)
+
+
+@app.exception_handler(TelegramBadRequest)
+async def error_handler(_request: Request, exc: TelegramBadRequest):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder({'error': str(exc)}),
+    )
+
 
 __all__ = [
     'app',
